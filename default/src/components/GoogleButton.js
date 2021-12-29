1f057ff01c1c88ecb3071d6840ea9b4c3e491073
Jsgithubchannel <jsschannel99@gmail.com>
2021-12-29 20:34:14 +0900

    add login reducer in google login logic

--- default/src/components/GoogleButton.js
+++ default/src/components/GoogleButton.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GoogleLogin from "react-google-login";
+
+import { useDispatch } from "react-redux";
 
 const clientId = process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID;
 
 export default function GoogleButton({ history }) {
-  const onSuccess = async (response) => {
-    console.log(response);
+  const dispatch = useDispatch();
 
+  const onSuccess = async (response) => {
     const access_token = response.accessToken;
+
+    dispatch({
+      type: "login",
+      payload: {
+        isLoggedIn: true,
+        access_token: response.accessToken,
+        username: response.profileObj.name,
+        gender: "gender 정보 없음",
+        age: "age 정보 없음",
+        provider: "google",
+      },
+    });
+    console.log(response);
 
     // 구글 로그인 성공시 서버에 전달할 데이터
     // await onSocial({
--- default/src/pages/Review.js
+++ default/src/pages/Review.js
@@ -1,112 +1,110 @@
-import React, { useState } from 'react';
-import './ReviewList.css';
-import ReviewNav from '../components/ReviewNav';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import "./ReviewList.css";
+import ReviewNav from "../components/ReviewNav";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as fasFaHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as farFaHeart } from "@fortawesome/free-regular-svg-icons";
 
-
 function Review() {
-    let dispatch = useDispatch();
-    let today = new Date()
-    let currenrDate = today.getFullYear() + '.' + (today.getMonth() + 1) + '.' + today.getDate();
+  let dispatch = useDispatch();
+  let today = new Date();
+  let currenrDate =
+    today.getFullYear() + "." + (today.getMonth() + 1) + "." + today.getDate();
 
-    let [reviewData, setReviewData] = useState('');
+  let [reviewData, setReviewData] = useState("");
 
-    let [heartCount, setHeartCount] = useState(0);
+  let [heartCount, setHeartCount] = useState(0);
 
+  return (
+    <div>
+      <ReviewNav navTitle={"후기 등록"} />
+      <div className="review-title">피카소 미술관</div>
+      <p className="review">후기를 남겨주세요</p>
 
-    return (
-        <div>
-            <ReviewNav navTitle={'후기 등록'} />
-            <div className="review-title">피카소 미술관</div>
-            <p className="review">후기를 남겨주세요</p>
+      {/* <form className="review-form"> */}
+      <div className="review-heart">
+        <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
+        <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
+        <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
+        <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
+        <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
+      </div>
 
-            {/* <form className="review-form"> */}
-            <div className="review-heart">
-                <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
-                <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
-                <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
-                <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
-                <ReviewHeart heartCount={heartCount} setHeartCount={setHeartCount} />
-            </div>
-
-            <hr />
-            <div className="review-wrap">
-                <input
-                    type="text"
-                    placeholder="후기를 남겨주세요."
-                    className="review-textfield"
-                    onChange={(e) => {
-                        setReviewData(e.target.value);
-                    }}
-                />
-                <div className="review-file-wrap">
-                    <label className="review-file-label" for="review-file">📷 사진 추가하기</label>
-                    <input
-                        type="file"
-                        multiple
-                        id="review-file"
-                    />
-
-                </div>
-                <Link to='/review'>
-                    <button
-                        className="submit-btn review-btn"
-                        onClick={() => {
-                            dispatch({
-                                type: 'add',
-                                payload: { heart: heartCount, name: '상상상', date: currenrDate, content: reviewData, img: null },
-                            })
-                        }}
-                    >등록하기</button>
-                </Link>
-            </div>
-            {/* </form> */}
+      <hr />
+      <div className="review-wrap">
+        <input
+          type="text"
+          placeholder="후기를 남겨주세요."
+          className="review-textfield"
+          onChange={(e) => {
+            setReviewData(e.target.value);
+          }}
+        />
+        <div className="review-file-wrap">
+          <label className="review-file-label" for="review-file">
+            📷 사진 추가하기
+          </label>
+          <input type="file" multiple id="review-file" />
         </div>
-    )
+        <Link to="/review">
+          <button
+            className="submit-btn review-btn"
+            onClick={() => {
+              dispatch({
+                type: "add",
+                payload: {
+                  heart: heartCount,
+                  name: "상상상",
+                  date: currenrDate,
+                  content: reviewData,
+                  img: null,
+                },
+              });
+            }}
+          >
+            등록하기
+          </button>
+        </Link>
+      </div>
+      {/* </form> */}
+    </div>
+  );
 }
 
 export default Review;
 
 function ReviewHeart(props) {
+  let [colorState, setColorState] = useState(false);
 
-    let [colorState, setColorState] = useState(false);
-
-    return (
-        <div>
-            {
-                colorState === true
-                    ? (
-                        <>
-                            <FontAwesomeIcon
-                                icon={fasFaHeart}
-                                className="click-heart"
-                                size={'3x'}
-                                onClick={() => {
-                                    setColorState(!colorState)
-                                    props.setHeartCount(props.heartCount - 1)
-                                }}
-
-                            />
-                        </>
-                    )
-                    : (
-                        <>
-                            <FontAwesomeIcon
-                                icon={farFaHeart}
-                                className="unclick-heart"
-                                size={'3x'}
-                                onClick={() => {
-                                    setColorState(!colorState)
-                                    props.setHeartCount(props.heartCount + 1)
-                                }} />
-                        </>
-                    )
-            }
-
-        </div>
-    )
+  return (
+    <div>
+      {colorState === true ? (
+        <>
+          <FontAwesomeIcon
+            icon={fasFaHeart}
+            className="click-heart"
+            size={"3x"}
+            onClick={() => {
+              setColorState(!colorState);
+              props.setHeartCount(props.heartCount - 1);
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <FontAwesomeIcon
+            icon={farFaHeart}
+            className="unclick-heart"
+            size={"3x"}
+            onClick={() => {
+              setColorState(!colorState);
+              props.setHeartCount(props.heartCount + 1);
+            }}
+          />
+        </>
+      )}
+    </div>
+  );
 }
