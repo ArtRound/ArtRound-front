b783a642abe8f2e5d4b23e382bb6df05c491a35a
Jsgithubchannel <jsschannel99@gmail.com>
2022-01-04 15:36:09 +0900

    Feat: edit kakao login async to fetch

--- default/src/pages/Auth.js
+++ default/src/pages/Auth.js
@@ -8,11 +8,12 @@ export default function Auth() {
   const REDIRECT_URI = "http://localhost:3000/main/login/kakao";
   const CLIENT_SECRET = "9dFVNap0AB2XWwrros7HHpzQ8Hr8Txl7";
 
-  const code = new URL(window.location.href).searchParams.get("code");
-
   const history = useHistory();
 
   const getToken = async () => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    console.log(code);
+
     const payload = qs.stringify({
       grant_type: "authorization_code",
       client_id: REST_API_KEY,
@@ -20,29 +21,69 @@ export default function Auth() {
       code: code,
       client_secret: CLIENT_SECRET,
     });
-    try {
-      // access token 가져오기
-      const res = await axios.post(
-        "https://kauth.kakao.com/oauth/token",
-        payload
-      );
+    // access token 가져오기
+    const res = await axios.post(
+      "https://kauth.kakao.com/oauth/token",
+      payload
+    );
 
-      await axios.get("http://localhost:8000/main/login/kakao/finish", {
+    // const data = JSON.stringify(res.data.access_token);
+
+    // await axios
+    //   .post("http://localhost:8000/main/login/kakao/finish/", data, {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       //prettier-ignore
+    //       "Accept": "application/json",
+    //     },
+    //   })
+    //   .then((response) => {
+    //     console.log("성공", response);
+
+    //     // // Kakao Javascript SDK 초기화
+    //     // window.Kakao.init(REST_API_KEY);
+    //     // // access token 설정
+    //     // window.Kakao.Auth.setAccessToken(res.data.access_token);
+    //     // history.replace("/profile");
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.response.data);
+    //     console.log(err.response.status);
+    //     console.log(err.response.headers);
+    //   });
+
+    await fetch("http://localhost:8000/main/login/kakao/finish/", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
         params: {
           code: res.data.access_token,
         },
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.result === "true") {
+          console.log("success");
+        }
+        // else{
+        //     history.push({
+        //         pathname: "/signupprofile",
+        //         state: {uid:response.uid,email:response.email}
+        //     })
+        // }
+      })
+      .catch((err) => {
+        console.log(err);
       });
-
-      console.log(res);
-
-      // Kakao Javascript SDK 초기화
-      window.Kakao.init(REST_API_KEY);
-      // access token 설정
-      window.Kakao.Auth.setAccessToken(res.data.access_token);
-      history.replace("/profile");
-    } catch (err) {
-      console.log(err);
-    }
   };
 
   useEffect(() => {
