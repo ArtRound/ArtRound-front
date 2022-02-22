4b3a46985fcc0f684d5940b660e61c9255697b9e
siiiido <discegaudere24.7@gmail.com>
2022-02-22 14:39:13 +0900

    Style: change code format

--- default/src/App.js
+++ default/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, lazy, Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route } from "react-router-dom"; //React-Router import
-import axios from "axios";
 
 import MyPage from "./pages/Mypage";
 import UserFavorite from "./pages/UserFavorite";
@@ -20,61 +19,12 @@ import Review from "./pages/Review";
 import Information from "./pages/Information";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
-import Loader from "./components/Loader";
+import Map from "./pages/Map";
 
 import PublicRoute from "./services/PublicRoute";
 import PrivateRoute from "./services/PrivateRoute";
-import { useSelector } from "react-redux";
-
-let Map = lazy(() => {
-  return import("./pages/Map");
-});
-
-export let placeData = React.createContext();
 
 const App = () => {
-  const [markerState, setMarkerState] = useState(false);
-  const [datas, setDatas] = useState([]);
-
-  // useEffect(() => {
-  //   const URL = [];
-  //   const getURL = [];
-
-  //   for (let i = 0; i < 15; i++) {
-  //     URL[
-  //       i
-  //     ] = `http://api.data.go.kr/openapi/tn_pubr_public_museum_artgr_info_api?serviceKey=BfdusobQEjVcCsm1nVfc3AnA%2BsBih1Corc0TwKt9B%2Ft46CeONaFq%2Bn0%2BxkUnGO9fzeQHPLjXLLCk8aFpYejEbQ%3D%3D&pageNo=${
-  //       i + 1
-  //     }&numOfRows=100&type=json`;
-  //     getURL[i] = axios.get(URL[i]);
-  //   }
-
-  //   let copy = [];
-
-  //   axios
-  //     .all([...getURL])
-  //     .then((res) => {
-  //       res.map((v, i) => {
-  //         copy.push(...v.data.response.body.items);
-  //       });
-  //     })
-  //     .then(() => {
-  //       copy.map((item, i) => {
-  //         item.id = i;
-  //         // console.log(item, ' here is item');
-  //         setDatas((datas) => {
-  //           // console.log(datas, " here is datas");
-  //           return [...datas, item];
-  //         });
-  //         return item;
-  //       });
-  //       setMarkerState(true);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
-
   return (
     <div className="container">
       <BrowserRouter>
@@ -87,20 +37,11 @@ const App = () => {
         <PrivateRoute path="/profile">
           <Profile />
         </PrivateRoute>
+
         <PrivateRoute path="/mypage" component={MyPage} />
         <PrivateRoute path="/information" component={Information} />
-
-        <placeData.Provider value={datas}>
-          <PrivateRoute path="/detail/:id" component={Detail} />
-
-          <Suspense fallback={<Loader />}>
-            <PrivateRoute path="/map">
-              {" "}
-              <Map markerState={markerState} />{" "}
-            </PrivateRoute>
-          </Suspense>
-        </placeData.Provider>
-
+        <PrivateRoute path="/detail/:id" component={Detail} />
+        <PrivateRoute path="/map" component={Map} />
         <PrivateRoute path="/favorite" component={UserFavorite} />
         <PrivateRoute path="/visited" component={UserVisited} />
         <PrivateRoute path="/service" component={ServiceCenter} />
