bab85736234eaf1d1bc2c7c53ae6afbc4a3d63ca
jeryjayjae <hjk0639@pukyong.ac.kr>
2022-03-31 02:40:31 +0900

    Edit : send a parameter in GET request

--- default/src/pages/UserFavorite.js
+++ default/src/pages/UserFavorite.js
@@ -4,13 +4,15 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { ADD_DATA } from "../context/actionTypes";
 import { Context } from "../context/index";
+import { useSelector } from "react-redux";
 
 const UserFavorite = ({ history }) => {
+  const user_id = useSelector((state) => state.infoReducer.id);
   const [text, setText] = useState([]);
   function GetFavortie() {
     useEffect(() => {
       axios
-        .get("http://127.0.0.1:8000/main/favorites/")
+        .get(`http://127.0.0.1:8000/main/favorites/${user_id}`)
         .then((response) => {
           setText([...response.data]);
           console.log(response.data);
@@ -18,7 +20,7 @@ const UserFavorite = ({ history }) => {
         .catch(function (error) {
           console.log(error);
         });
-    }, []);
+    }, [user_id]);
   }
 
   const { dispatch } = useContext(Context);
